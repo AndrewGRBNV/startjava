93620d59275c6088d1be40bb1819476ab41dfe5d
Andrew GRBNV <stolet82@gmail.com>
2020-10-24 21:03:09 +0300

    commite 6

--- lesson_2/GuessNumberTest.java
+++ lesson_2/GuessNumberTest.java
@@ -3,7 +3,6 @@ class GuessNumberTest {
     public static void main(String[] args) {
         boolean runGuessNumberTest = true;
         while (runGuessNumberTest) {
-            String name = "firt";
             GuessNumber player = new GuessNumber();
             Player player_0 = new Player(player.playerName('1'));
             Player player_1 = new Player(player.playerName('2'));
